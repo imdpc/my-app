cea82308fedf20e7049c9ac3daa3766866488ab7
Prasad Adhav <prasadadhav22@gmail.com>
2024-03-19 10:53:06 +0530

    update 7 files

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,23 +1,56 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonSearchbar,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle className="for-my-header-page">Tab 1 hi</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <div
+          className="main-container-for-card ion-padding ion-text-center
+        "
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores
+            itaque hic inventore, sit ducimus expedita cumque mollitia natus
+            possimus explicabo debitis quisquam quia vel excepturi dolor
+            doloremque rerum corporis! Laboriosam?
+          </p>
+        </div>
       </IonContent>
+      {/* <IonSearchbar placeholder="Custom Placeholder"></IonSearchbar>{" "} */}
+      <IonCard>
+        <img
+          alt="Silhouette of mountains"
+          src="https://ionicframework.com/docs/img/demos/card-media.png"
+        />
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Here's a small text description for the card content. Nothing more,
+          nothing less.
+        </IonCardContent>
+      </IonCard>
     </IonPage>
   );
 };
